c2b248f028557c4e426d4956df0bf6e9b75ac303
Farhan Mohammed <farhanhm12@gmail.com>
2019-10-19 20:46:59 -0400

    Updates

---
+++ precache-manifest.99d2e6e26eee4694025110962cac4f89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47c07c43c3848845c581f574a2578c82",
+    "url": "/Search/index.html"
+  },
+  {
+    "revision": "9fa2bab76734a6f8f034",
+    "url": "/Search/static/css/main.b430206a.chunk.css"
+  },
+  {
+    "revision": "01f001c735625cba4d20",
+    "url": "/Search/static/js/2.fc36bde1.chunk.js"
+  },
+  {
+    "revision": "9fa2bab76734a6f8f034",
+    "url": "/Search/static/js/main.c849e2a9.chunk.js"
+  },
+  {
+    "revision": "6811c5ed8435a2bddbb8",
+    "url": "/Search/static/js/runtime~main.c15c92eb.js"
+  }
+]);
